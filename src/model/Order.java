9c0143a1d250f25850ab6d24344fa6881c603e31
Himanshuram Ranjan <himanshuramr@gmail.com>
2025-07-13 21:49:58 +0530

    added some boolean checks for order type

--- src/model/Order.java
+++ src/model/Order.java
@@ -3,31 +3,36 @@ package model;
 import enums.OrderStatus;
 import enums.OrderType;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Order {
     private static AtomicInteger counter = new AtomicInteger(0);
     private final String orderId;
     private OrderType orderType;
-    private Stock stock;
+    private final Stock stock;
     private int quantity;
-    private double price;
+    private Double price;
     private OrderStatus orderStatus;
+    private final  LocalDateTime orderTime;
+    private final Investor investor;
 
-    public Order(OrderType orderType, Stock stock, int quantity, double price) {
+    public Order(Investor investor, OrderType orderType, Stock stock, int quantity, Double price) {
         this.orderId = "ORD".concat(String.valueOf(counter.getAndIncrement()));
+        this.investor = investor;
         this.orderType = orderType;
         this.stock = stock;
         this.quantity = quantity;
         this.price = price;
         this.orderStatus = OrderStatus.PENDING;
+        this.orderTime = LocalDateTime.now();
     }
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 
-    public void setPrice(double price) {
+    public void setPrice(Double price) {
         this.price = price;
     }
 
@@ -43,7 +48,7 @@ public class Order {
         return quantity;
     }
 
-    public double getPrice() {
+    public Double getPrice() {
         return price;
     }
 
@@ -53,5 +58,29 @@ public class Order {
 
     public OrderType getOrderType() {
         return orderType;
+    }
+
+    public LocalDateTime getOrderTime() {
+        return orderTime;
+    }
+
+    public void setOrderType(OrderType orderType) {
+        this.orderType = orderType;
+    }
+
+    public Investor getInvestor() {
+        return investor;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public boolean isBuyOrder() {
+        return getOrderType().equals(OrderType.MARKET_BUY) || getOrderType().equals(OrderType.LIMIT_BUY);
+    }
+
+    public boolean isMarketOrder() {
+        return getOrderType().equals(OrderType.MARKET_BUY) || getOrderType().equals(OrderType.MARKET_SELL);
     }
 }
